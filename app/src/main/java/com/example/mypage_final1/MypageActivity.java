9df75e8106672398f42c392ceccbf52ec63221b4
vv970 <vv9702@naver.com>
2021-06-06 23:02:23 +0900

    d

---
+++ app/src/main/java/com/example/mypage_final1/MypageActivity.java
@@ -0,0 +1,14 @@
+package com.example.mypage_final1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MypageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_tab);
+    }
+}
